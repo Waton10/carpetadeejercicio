849ec1fb8beab62712abd15474d32fb67250c526
Waton10 <142358537+Waton10@users.noreply.github.com>
2024-08-17 12:48:56 -0600

    Delete conversordeMetrosaKilometros.py

--- conversordeMetrosaKilometros.py
+++
@@ -1,8 +0,0 @@
-def metros_a_kilometros(metros):
-    kilometros = metros / 1000
-    return kilometros
-
-cantidad_metros = float(input("Ingrese una cantidad en metros: "))
-cantidad_kilometros = metros_a_kilometros(cantidad_metros)
-
-print(f"{cantidad_metros} metros son equivalentes a {cantidad_kilometros} kilómetros")
