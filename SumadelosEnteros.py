14461d31f5ca1b88b4bb7991578ca321796753b1
Waton10 <142358537+Waton10@users.noreply.github.com>
2024-08-17 12:48:32 -0600

    Delete SumadelosEnteros.py

--- SumadelosEnteros.py
+++
@@ -1,9 +0,0 @@
-# Inicializar una variable para almacenar la suma
-suma_total = 0
-
-# Iterar a través de los números del 1 al 100
-for num in range(1, 101):
-    suma_total += num
-
-# Imprimir el resultado
-print("La suma de los enteros del 1 al 100 es:", suma_total)
