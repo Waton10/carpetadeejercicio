7e7fa78ee63379b1b75144a67d2dc342758ceb1f
Waton10 <142358537+Waton10@users.noreply.github.com>
2024-08-17 12:49:36 -0600

    Delete ejemplo3.py

--- ejemplo3.py
+++
@@ -1,9 +0,0 @@
-# Ejemplo de una lista de diccionarios de tuplas
-lista_anidada = [
-    {'nombre': 'Juan', 'puntuaciones': [(10, 'Matemáticas'), (8, 'Ciencias')]},
-    {'nombre': 'María', 'puntuaciones': [(9, 'Matemáticas'), (7, 'Ciencias')]}
-]
-
-# Acceso a elementos en la estructura de datos
-puntuacion_juan_mate = lista_anidada[0]['puntuaciones'][0]  # Tupla (10, 'Matemáticas')
-nombre_maria = lista_anidada[1]['nombre']  # Nombre 'María'
